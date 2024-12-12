9b153b4251c9a2902541218c75a8836209fd0fc9
Mariah <alinemariah@hotmail.com>
2024-12-12 16:20:26 +0100

    actualization day 1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,15 @@ import React from 'react';
 
 const Footer = () => {
     return (
+        <>
+        <footer style = {{textAlign: 'center'}}>
+            <a href="" style={{}}>
+            
+
+            </a>
+            <p>Hello</p>
+        </footer>
+        </>
 
     );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 
 const Navbar = () => {
     return (
+        <>
+    <div className="App">
+      <div className="nav">
+        <img className="logo" src="./src/assets/img/image.png"></img>
+        <p className="appName">To Do App</p>
+
+      </div>
+      </div>
+
+        </>
 
     );
 };
