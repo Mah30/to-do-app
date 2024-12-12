329ca59d3778964a2b3ce27257acaaef941bc162
Mariah <alinemariah@hotmail.com>
2024-12-12 10:56:27 +0100

    actualization components Folders again

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+
+import React from 'react';
 import { useState } from 'react'
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
+import Home from './components/Home';
+
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+
 
 function App() {
   const [count, setCount] = useState(0)
